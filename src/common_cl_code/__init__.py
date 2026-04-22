c51e52b1665d3836f4c72f2adc219bb7cc3cc99f
Jonathan Gould <jonathan.d.gould@gmail.com>
2026-04-22 11:29:20 -0400

    Add latent space analysis notebook and library-level coordination code

--- src/common_cl_code/__init__.py
+++ src/common_cl_code/__init__.py
@@ -9,14 +9,19 @@ except PackageNotFoundError:
 
 import socket
 import pathlib
+import cl
+
+running_on_real_device = 'sdk' not in cl.get_system_attributes()['system_id']
 
 hostname = socket.gethostname()
 if hostname == 'tycho':
+    assert not running_on_real_device
     datasets_base_path = pathlib.Path("/mnt/data/")
     recording_base_path = datasets_base_path / "cl-sdk_output"
 elif 'CL1' in hostname:
     datasets_base_path = pathlib.Path("/home/labuser/storage/")
     recording_base_path = pathlib.Path("/home/labuser/recordings/")
+    assert running_on_real_device
 else:
     raise ValueError(f'Hostname {socket.gethostname()} not recognized.')
 
